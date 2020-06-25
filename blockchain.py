72ac2b0d0d9c08ee6c68348489cd6ddb61164760
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2020-06-26 00:15:47 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -45,8 +45,21 @@ class blokchain:
              if block['previous_hash'] != self.hash(previous_block):
                  return False
              previous_proof = previous_block['proof']
-             
+             proof = block['proof']
+             hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).enocode()).hexdigest()
+             if hash_operation[:4] != '0000':
+                 return False
+             previous_block = block
+             block_index += 1
+             return True
+    
     
  #mining our blockchain
+        # creating a web app
+       
+        
+       
+        
+       # 
         
            
