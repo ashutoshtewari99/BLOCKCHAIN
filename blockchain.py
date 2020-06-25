8c8938e38ee70209e027731e19e7f20abcf03a4e
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2020-06-25 20:54:35 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -11,7 +11,7 @@ class blokchain:
         self.create_block(proof = 1, previous_hash = '0') 
         
     def create_block(self, proof, previous_hash):
-        block = {'index' : = len(self.chain) + 1,
+        block = {'index' : len(self.chain) + 1,
                  'timestamp' : str(datetime.datetime.now()),
                  'proof': proof,
                  'previous_hash': previous_hash}  
@@ -27,12 +27,25 @@ class blokchain:
         
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).enocode()).hexdigest()
-            if hash_operation(:4) == '0000':
-            check_proof = True
+            if hash_operation[:4] == '0000':
+                check_proof = True
             else:
-            new_proof += 1
-            
+                new_proof += 1   
         return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+         previous_block = chain[0]
+         block_index = 1
+         while block_index < len(chain):
+             block = chain[block_index]
+             if block['previous_hash'] != self.hash(previous_block):
+                 return False
+             previous_proof = previous_block['proof']
+             
     
  #mining our blockchain
         
